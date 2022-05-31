aad5af2306918b579dfe3b278c3f84b3112dcff1
Sam Wallace <sam@sammymayhem.com>
2022-05-30 19:59:25 -0700

    About page almost done, contact page almost done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Form, FormControl, Button, InputGroup } from 'react-bootstrap'
+import '../styles/Contact.css'
 
 export const Contact = () => {
     return (
-        <div>
-            <h4>Contact me section</h4>
+        <div id="contact-form">
+            <div id="contact-title">
+                <h1>Contact Me</h1>
+            </div>
+            <Form>
+                <Form.Group className="mb-3" id="name" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter your name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" id="email" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <InputGroup id="user-text">
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+                <Button variant="outline-info" id="submit-button" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
