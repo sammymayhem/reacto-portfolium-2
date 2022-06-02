e126c2feb90235a360be60c0c12ac07b21ffd3c1
Sam Wallace <sam@sammymayhem.com>
2022-06-01 18:07:38 -0700

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import '../styles/Footer.css';
+
+export const Footer = () => {
+    return (
+        <div className="sticky-bottom footer">
+            <Navbar color="dark" dark>
+                <Container>
+                    <div><a href="https://github.com/sammymayhem"><FaGithub /></a></div>
+                    <div className="linked-in"><a href="https://www.linkedin.com/in/sam-wallace-92b6238/"><FaLinkedin /></a></div>
+                    <div><a href="https://www.facebook.com/sammymayhem/"><FaFacebook /></a></div>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
